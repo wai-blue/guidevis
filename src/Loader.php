6777784090518267f72fa5bfbe6279d47744c2cb
Dusan <dusan.daniska@wai.sk>
2024-11-08 13:56:56 +0100

    updated loadPagesFromContent

--- src/Loader.php
+++ src/Loader.php
@@ -87,6 +87,21 @@ class Loader {
       }
     }
 
+    foreach ($pages as $page => $pageData) {
+      $content = $this->getPageContent($page);
+
+      $lines = explode("\n", $content);
+      foreach ($lines as $line) {
+        $line = trim($line);
+        if (\str_starts_with($line, "# ")) {
+          $pages[$page]['title'] = trim($line, '# ');
+          break;
+        }
+      }
+
+      if (empty($pages[$page]['title'])) $pages[$page]['title'] = $page;
+    }
+
     return $pages;
   }
 
